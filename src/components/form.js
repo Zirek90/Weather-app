795e123d0a19bbb7a166a61ffe3c979e6083aa20
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-06-27 21:43:54 +0200

    fixed h1

--- src/components/form.js
+++ src/components/form.js
@@ -8,7 +8,7 @@ class Form extends React.Component {
       render() {
         return (
             <form onSubmit={this.props.getWeather}>
-                <h1>Weather conditions in your area</h1>
+                <h1>Weather conditions in chosen city</h1>
                 <input type="text" name="city" placeholder="Warsaw" autoComplete="off"/>
                 <input type="text" name="country" placeholder="Poland" autoComplete="off"/>
                 <button type="submit">Choose city</button>
