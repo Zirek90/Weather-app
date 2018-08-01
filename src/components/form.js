854c3afbb8f58f788a804965589bd2f24e4f1bda
Mateusz Grzymowicz <mateusz.grzymowicz@gmail.com>
2018-08-01 15:33:16 +0200

    fixed placeholder

--- src/components/form.js
+++ src/components/form.js
@@ -9,8 +9,8 @@ class Form extends React.Component {
         return (
             <form onSubmit={this.props.getWeather}>
                 <h1>Weather conditions in chosen city</h1>
-                <input type="text" name="city" placeholder="Warsaw" autoComplete="off"/>
-                <input type="text" name="country" placeholder="Poland" autoComplete="off"/>
+                <input type="text" name="city" placeholder="city" autoComplete="off"/>
+                <input type="text" name="country" placeholder="country" autoComplete="off"/>
                 <button type="submit">Choose city</button>
             </form>
 
